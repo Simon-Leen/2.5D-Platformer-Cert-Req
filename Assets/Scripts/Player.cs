1feba5873647db49e11c29434fd2dfcf81262e00
= <=>
2021-08-26 06:04:51 +0100

    Player imported and idle anim

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _cc;
+    private Vector3 _direction;
+    [SerializeField] private float _speed = 12.0f;
+    [SerializeField] private float _gravity = 30.0f;
+    [SerializeField] private float _jumpH = 15.0f;
+
+    void Start()
+    {
+        _cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if(_cc.isGrounded == true)
+        {
+            float h = Input.GetAxis("Horizontal");
+            _direction = new Vector3(0, 0, h) * _speed;
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _direction.y += _jumpH;
+            }
+        }
+        _direction.y -= _gravity * Time.deltaTime;
+
+        _cc.Move(_direction * Time.deltaTime);
+    }
+}
